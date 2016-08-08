57ae285b40a08e2281eadd60513ac4269f52e23d
李延博 <liyanbo6666@163.com>
2016-08-08 19:00:24 +0800

    no message

--- VDCameraAndPhotoTool/VDCameraAndPhotoTool/VDCameraAndPhotoTool.h
+++ VDCameraAndPhotoTool/VDCameraAndPhotoTool/VDCameraAndPhotoTool.h
@@ -9,26 +9,27 @@
 #import <UIKit/UIKit.h>
 
 typedef void(^cameraReturn)(UIImage *image);
-
+typedef void(^videoReturn)(NSURL *videoUrl);
 
 @interface VDCameraAndPhotoTool : NSObject<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 
 + (instancetype)shareInstance;
 
+- (void)showVideoInViewController:(UIViewController *)vc andFinishBack:(videoReturn)finishBack;
 /**
  *  调用系统相机
  *
  *  @param vc         要调用相机的控制器
  *  @param finishBack 拍照完成的回调
  */
-- (void)showCameraInViewComtroller:(UIViewController *)vc andFinishBack:(cameraReturn)finishBack;
+- (void)showCameraInViewController:(UIViewController *)vc andFinishBack:(cameraReturn)finishBack;
 /**
  *  调用系统相册
  *
  *  @param vc         要调用相册的控制器
  *  @param finishBack 选择完成的回调
  */
-- (void)showPhotoInViewComtroller:(UIViewController *)vc andFinishBack:(cameraReturn)finishBack;
+- (void)showPhotoInViewController:(UIViewController *)vc andFinishBack:(cameraReturn)finishBack;
 
 /**
  *  显示相机或相册（弹出alert）
